ae7c3122e29da13a81291670c3a443909a115b3e
bzyyy <brian.montero@sansano.usm.cl>
2023-05-10 00:27:08 -0400

    fix cors dependences

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import koa from 'koa'
 import bodyParser from 'koa-body'
 import router from './routes/index'
 
+const cors = require('@koa/cors');
+
 const app = new koa()
+
+app.use(cors())
 
 app.use(bodyParser({ multipart: true, urlencoded: true }))
 app.use(router.routes())
